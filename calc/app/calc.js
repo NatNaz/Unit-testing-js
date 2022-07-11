47871c100e2c796720f6fa7f9060b1b72d76bfb9
Natalie <3362282@gmail.com>
2022-07-11 22:27:59 +0300

    upgrate code in app/calc

--- calc/app/calc.js
+++ calc/app/calc.js
@@ -1,23 +1,25 @@
 //A simple class containing methods for calculate numbers
-class calculator {
+class Сalculator {
   //Creates an instance of calculator.
 
   constructor() {}
 
+  validateParameters(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+      throw new Error(
+        `Cannot run the task as at leasts one of the arguments is not of type "Number"`
+      );
+    }
+  }
+
   /*@param {Number} a
    *@param {Number} b
    *@return {Number} sum of 2 numbers
-   *@memberof calculator
+   *@memberof Calculator
    */
   add(a, b) {
-    if (typeof a !== "number" || typeof b !== "number") {
-      throw new Error(
-        `Cannot count the sum as at leats one of the arguments is not of type "Number"`
-      );
-    } else {
-      let sum = a + b;
-      return sum;
-    }
+    this.validateParameters(a, b);
+    return a + b;
   }
 
   /*@param {Number} a
@@ -26,19 +28,9 @@ class calculator {
    *@memberof calculator
    */
   multiply(a, b) {
-    if (typeof a !== "number") {
-      throw new Error(
-        `Cannot count the product as the first arguments is not of type "Number"`
-      );
-    } else if (typeof b !== "number") {
-      throw new Error(
-        `Cannot count the product as the second arguments is not of type "Number"`
-      );
-    } else {
-      let product = a * b;
-      return product;
-    }
+    this.validateParameters(a, b);
+    return a * b;
   }
 }
 
-module.exports = calculator;
+module.exports = Сalculator;
