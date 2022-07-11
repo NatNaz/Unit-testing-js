0b32a2b459cd0f512e64324fa25c077dd6a76dcf
Natalie <3362282@gmail.com>
2022-07-11 22:26:37 +0300

    fix spec multiply

--- calc/test/specs/multiply.spec.js
+++ calc/test/specs/multiply.spec.js
@@ -1,4 +1,4 @@
-const multiply = require("../../app/calc.js");
+const Calculator = require("../../app/calc.js");
 const chai = require("chai");
 const { expect } = require("chai");
 const { describe } = require("mocha");
@@ -6,13 +6,11 @@ const spies = require("chai-spies");
 chai.use(spies);
 
 describe("multiply scenarios", function () {
-  let calculator, spy;
+  let calculator;
+  let spy;
   this.beforeEach(function () {
-    calculator = new multiply();
+    calculator = new Calculator();
     spy = chai.spy.on(calculator, "multiply");
-  });
-  this.afterEach(function () {
-    calculator = null;
   });
   it("should return product of numbers", function () {
     expect(calculator.multiply(4, 3)).to.be.equal(12);
@@ -21,14 +19,14 @@ describe("multiply scenarios", function () {
     let callWithErrorTypeOf = calculator.multiply(true, 12);
     expect(spy).to.be.a.spy;
     expect(callWithErrorTypeOf).to.throw(
-      `Cannot count the product as the first arguments is not of type "Number"`
+      `Cannot run the task as at leasts one of the arguments is not of type "Number"`
     );
   });
   it("should throw an error if second agument is not of type 'Number'", function () {
     let callWithErrorTypeOf = calculator.multiply(10, false);
     expect(spy).to.be.a.spy;
     expect(callWithErrorTypeOf).to.throw(
-      `Cannot count the product as the second arguments is not of type "Number"`
+      `Cannot run the task as at leasts one of the arguments is not of type "Number"`
     );
   });
 });
